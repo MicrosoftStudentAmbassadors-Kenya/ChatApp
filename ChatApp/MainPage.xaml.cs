72a9cab8ba793b2869f9cebc18088f5e10313154
Aaron Kipkoech <aaronrono42@gmail.com>
2020-02-12 12:54:20 +0300

    Login Page and naviagtions

---
+++ ChatApp/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ChatApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Clicked(object sender, EventArgs e)
+        {
+
+            if (EmailEntry.Text == null || PasswordEntry.Text == null)
+           { 
+            
+            
+            }
+            else
+            {
+                Navigation.PushAsync(new HomePage());
+            }
+            
+        }
+    }
+}
